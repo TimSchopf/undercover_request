5c9a909755595a9c06ce6a4a43d3ee72a6459e71
Tim Schopf <tim.schopf@t-online.de>
2019-04-16 09:58:42 +0200

    added dependencies

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from distutils.core import setup
-setup(name='undercover_request', version='1.0', author='Tim Schopf', author_email='tim.schopf@t-online.de', license='MIT License', url='https://github.com/TimSchopf/undercover_request', py_modules=['undercover_request'])
+setup(name='undercover_request', version='1.0', author='Tim Schopf', author_email='tim.schopf@t-online.de', license='MIT License', url='https://github.com/TimSchopf/undercover_request', py_modules=['undercover_request'], install_requires=['beautifulsoup4>=4.6.0','fake-useragent>=0.1.11','random','requests>=2.18.4'])
